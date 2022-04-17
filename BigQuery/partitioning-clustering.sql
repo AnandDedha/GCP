5ca5432533f18400c046bf7dc72ac8626f86f72f
Anand Kumar <ananddedha@gmail.com>
2022-04-17 09:46:32 -0400

    Create partitioning-clustering.sql

---
+++ BigQuery/partitioning-clustering.sql
@@ -0,0 +1,12 @@
+#standardSQL
+CREATE OR REPLACE TABLE `stackoverflow.questions_2018_clustered`
+PARTITION BY
+  DATE(creation_date)
+CLUSTER BY
+  tags AS
+SELECT
+  id, title, accepted_answer_id, creation_date, answer_count , comment_count , favorite_count, view_count, tags
+FROM
+  `bigquery-public-data.stackoverflow.posts_questions`
+WHERE
+  creation_date BETWEEN '2018-01-01' AND '2019-01-01';
