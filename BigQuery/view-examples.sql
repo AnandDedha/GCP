c93560f0c08621a12919d07828793c86995ec0af
Anand Kumar <ananddedha@gmail.com>
2022-05-01 15:52:33 -0400

    Update view-examples.sql

--- BigQuery/view-examples.sql
+++ BigQuery/view-examples.sql
@@ -1,10 +1,10 @@
 CREATE VIEW views_demo.male_names(name, number) AS
-    SELECT
+SELECT
       name,
-      number
+      count
     FROM
       `bigquery-demo-347418.Views_Demo.names`
     WHERE
       Gender = "M"
     ORDER BY
-      number DESC
+      count DESC
