f642a7236ec43521ee0bf32b5f2722afcbfa5d46
Anand Kumar <ananddedha@gmail.com>
2022-05-08 14:39:21 -0400

    Update rountines.sql

--- BigQuery/rountines.sql
+++ BigQuery/rountines.sql
@@ -1,4 +1,11 @@
-## Stored procedure
+## Stored Procedure Syntax
+CREATE PROCEDURE dataset_name.procedure_name
+BEGIN
+-- statements here
+END
+
+
+## Stored procedure example
 CREATE OR REPLACE PROCEDURE udf.create_customer()
 BEGIN
   DECLARE id STRING;
@@ -8,7 +15,26 @@ BEGIN
   SELECT FORMAT("Created customer %s", id);
 END
 
-## UDf
+## Temporary UDf
+CREATE TEMP FUNCTION cleanse_string (text STRING)
+  RETURNS STRING
+  AS (REGEXP_REPLACE(LOWER(TRIM(text)), '[^a-zA-Z0-9 ]+', ''));
+WITH strings AS
+  (SELECT '  Hello, World!!!  ' AS text
+  UNION ALL
+  SELECT ' I am $Special$ STrINg ' AS text
+  UNION ALL
+  SELECT ' John, Doe ' AS text)
+SELECT text
+     , cleanse_string(text) AS clean_text
+FROM strings;
+
+## Persistent udf
+
+CREATE OR REPLACE FUNCTION
+  udf.cleanse_string_test (text STRING)
+  RETURNS STRING
+  AS (REGEXP_REPLACE(LOWER(TRIM(text)), '[^a-zA-Z0-9 ]+', ''));
 
 
 
