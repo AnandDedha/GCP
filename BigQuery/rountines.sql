477584cfe8982246f920ec33f9f040ec278ff1ed
Anand Kumar <ananddedha@gmail.com>
2022-05-08 12:08:27 -0400

    Create rountines.sql

---
+++ BigQuery/rountines.sql
@@ -0,0 +1,11 @@
+## Stored procedure
+CREATE OR REPLACE PROCEDURE udf.create_customer()
+BEGIN
+  DECLARE id STRING;
+  SET id = GENERATE_UUID();
+  INSERT INTO udf.customers (id)
+    VALUES(id);
+  SELECT FORMAT("Created customer %s", id);
+END
+
+## UDf
