9c99eb4bdf08cbefd179d50673213105181e0c53
Anand Kumar <ananddedha@gmail.com>
2022-05-08 14:59:34 -0400

    Update rountines.sql

--- BigQuery/rountines.sql
+++ BigQuery/rountines.sql
@@ -15,7 +15,34 @@ BEGIN
   SELECT FORMAT("Created customer %s", id);
 END
 
-## Temporary UDf
+
+## Persistent udf
+
+CREATE OR REPLACE FUNCTION
+  udf.cleanse_string_test (text STRING)
+  RETURNS STRING
+  AS (REGEXP_REPLACE(LOWER(TRIM(text)), '[^a-zA-Z0-9 ]+', ''));
+  
+WITH strings AS
+  (SELECT '  Hello, World!!!  ' AS text
+  UNION ALL
+  SELECT ' I am $Special$ STrINg ' AS text
+  UNION ALL
+  SELECT ' John, Doe ' AS text)
+SELECT text
+     , `bigquery-demo-347418.udf.cleanse_string_test`(text) AS clean_text
+FROM strings;
+
+
+## Table Valued function
+CREATE OR REPLACE TABLE FUNCTION udf.names_by_year(y INT64)
+AS
+  SELECT year, name, SUM(number) AS total
+  FROM `bigquery-public-data.usa_names.usa_1910_current`
+  WHERE year = y
+  GROUP BY year, name
+  
+ ## Temporary UDf
 CREATE TEMP FUNCTION cleanse_string (text STRING)
   RETURNS STRING
   AS (REGEXP_REPLACE(LOWER(TRIM(text)), '[^a-zA-Z0-9 ]+', ''));
@@ -28,22 +55,6 @@ WITH strings AS
 SELECT text
      , cleanse_string(text) AS clean_text
 FROM strings;
-
-## Persistent udf
-
-CREATE OR REPLACE FUNCTION
-  udf.cleanse_string_test (text STRING)
-  RETURNS STRING
-  AS (REGEXP_REPLACE(LOWER(TRIM(text)), '[^a-zA-Z0-9 ]+', ''));
-
-
-## Table Valued function
-CREATE OR REPLACE TABLE FUNCTION udf.names_by_year(y INT64)
-AS
-  SELECT year, name, SUM(number) AS total
-  FROM `bigquery-public-data.usa_names.usa_1910_current`
-  WHERE year = y
-  GROUP BY year, name
   
 ## TVF example
 SELECT * FROM mydataset.names_by_year(1950)
